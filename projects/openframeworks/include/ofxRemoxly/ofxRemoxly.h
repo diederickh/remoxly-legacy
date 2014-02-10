8744f51144aec7a55fb4a1f98e5bfc33f2960650
roxlu <diederick@apollomedia.nl>
2014-02-11 00:28:47 +0100

    Packaging ofxRemoxly for mac

--- projects/openframeworks/include/ofxRemoxly/ofxRemoxly.h
+++ projects/openframeworks/include/ofxRemoxly/ofxRemoxly.h
@@ -3,9 +3,43 @@
   ofxRemoxly
   -----------
 
+  ofxRemoxly is a wrapper around the [Remoxly GUI](http://remoxly.roxlu.com). 
+  Remoxly is a clean, utilitarian GUI with basic gui widgets like toggles, 
+  buttons, sliders (int and float), text input and a basic color picker. 
 
+  Remoxly allows you to control your GUI through a remote connection. 
+
+  <example>
+
+    gui.addGroup("Particles");
+    gui.addColor("Color", color);
+    gui.addSliderFloat("Speed", particle_speed, 0.0, 10.0, 0.1);
+    gui.addSliderFloat("Trail width", trail_width, 0.0, 100.0, 0.01);
+    gui.addSliderInt("Trail length", trail_size, 0, 500, 1);
+    gui.addSliderInt("Circle Radius", circle_radius, 0, 300, 1);
+    gui.addToggle("Enable particles", enable_particles);
+    gui.addToggle("Render particles", render_particles);
+    gui.addToggle("Render trails", render_trails);
+    gui.addToggle("Render circle", render_circle);
   
+    gui.addGroup("Save and Load");
+    gui.addButton("Love", 0, GUI_ICON_HEART, on_button_click, this, 1);
+    gui.addButton("Save", 1, GUI_ICON_FLOPPY_O, on_button_click, this, 1);
+    gui.addButton("Load", 2, GUI_ICON_REFRESH, on_button_click, this,2);
+  
+    gui.addPanel();
+    gui.addGroup("Mesh");
+    gui.addSliderInt("Number of triangles", number_of_triangles, 0, 300, 3);
+    gui.addSliderFloat("Minimum Triangle Space", min_triangle_space, 0.0f, 100.0f, 1.0f);
+    gui.addToggle("Triangulate", triangulate);
+    gui.addText("Profile name", profile_name, 180);
+  
+    gui.enableRemoteAccess(2244);
+  
+    gui.load("gui.xml");
 
+  </example>
+  
  */
 #ifndef REMOXLY_OF_REMOXLY_H
 #define REMOXLY_OF_REMOXLY_H
